100fbe15daaeba5006ee7b476a7d9debae0b4076
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-10-26 14:30:53 -0400

    FIX missing education categories. simpler merge

--- 01_pull_ACS.R
+++ 01_pull_ACS.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(foreach)
 library(glue)
+library(labelled)
 library(tidycensus)
 
 
@@ -61,29 +62,35 @@ ages  <- c("18 to 24 years",
            "45 to 64 years",
            "65 years and over")
 age_lbl <- setNames(1:5L, ages)
-age_key <- tibble(1:5L,
-                  age_chr = ages,
+age_key <- tibble(age_chr = ages,
                   age = labelled(1:5L, age_lbl))
 
-education <- c("9th to 12th grade no diploma",
+education <- c("Less than 9th grade",
+               "9th to 12th grade, no diploma",
                "High school graduate \\(includes equivalency\\)",
-               "Some college no degree",
+               "Some college, no degree",
                "Associate's degree",
                "Bachelor's degree",
                "Graduate or professional degree")
 
-educ_lbl <- setNames(1:6L,
-              c("No HS", "High School Graduate", "Some College",
+educ_lbl <- setNames(1L:7L,
+              c("Less than 9", "No HS", "High School Graduate", "Some College",
               "2-Year", "4-Year", "Post-Grad"))
-educ_key  <- tibble(num = 1:6L,
-                    education = replace(education,  num ==  2L, "High school graduate (includes equivalency)"),
-                    educ_chr = names(educ_lbl),
-                    educ = labelled(1:6L, educ_lbl))
+educ_lbl_cces <- setNames(1L:6L, names(educ_lbl)[2:7])
 
-gender_key <- tibble(num = 1:2L,
-                     gender_chr = c("Male", "Female"),
+# CCES lumps the first two
+cces_lbl <- tibble(cces_label = names(educ_lbl)[2:7],
+                   educ = labelled(1:6L, educ_lbl_cces))
+
+educ_key  <- tibble(num = 1:7L,
+                    educ_chr = replace(education, num ==  3L, "High school graduate (includes equivalency)"),
+                    educ_fct = factor(names(educ_lbl), levels = names(educ_lbl))) %>%
+  mutate(cces_label = as.character(fct_collapse(educ_fct, `No HS` = c("Less than 9", "No HS")))) %>%
+  left_join(cces_lbl, by = "cces_label") %>%
+  select(-num)
+
+gender_key <- tibble(gender_chr = c("Male", "Female"),
                      gender = labelled(1:2L, c(Male = 1, Female = 2)))
-
 
 # get vars ----
 vars <- load_variables(2017, "acs1", cache = TRUE) %>%
@@ -96,32 +103,36 @@ vars <- load_variables(2017, "acs1", cache = TRUE) %>%
 # Clean dataset as the combination of the two ----
 cell_vars <- vars %>%
   filter(!is.na(gender), !is.na(age), !is.na(educ)) %>%
-  select(variable = name, gender, age, educ)
+  rename(variable = name, gender_chr = gender, age_chr = age, educ_chr = educ) %>%
+  left_join(gender_key, by = "gender_chr") %>%
+  left_join(age_key, by = "age_chr") %>%
+  left_join(educ_key, by = "educ_chr") %>%
+  select(variable, gender, age, educ)
 
-clean_strat <- function(grp_tab, codes = cell_vars) {
-  stopifnot(is.grouped_df(grp_tab))
+clean_strat <- function(grp_tab, geo, codes = cell_vars) {
+  stopifnot(is.grouped_df(grp_tab)) # grouped by vars for geo
 
   grp_tab %>%
-    mutate(sum_dist = sum(count*(variable == "B15001_001"))) %>% # sum
-    mutate(frac_dist = count / sum_dist) %>%
+    mutate(count_geo = sum(count*(variable == "B15001_001"))) %>% # sum geo
+    inner_join(codes, by = "variable") %>% # drop marginal cells, so cells form partition
+    group_by(year, gender, age, educ, count_geo, add = TRUE) %>%  # drop variable code (let our labels preceed)
+    summarize(count = sum(count)) %>%
     ungroup() %>%
-    inner_join(cell_vars, by = "variable") %>%
-    rename(education = educ, age_chr = age, gender_chr = gender) %>%
-    mutate(education = str_replace_all(education, "\\(includes equivalency\\)", "(includes equivalency)")) %>%
-    inner_join(select(educ_key, education, educ), by = "education") %>%
-    inner_join(select(age_key, age_chr, age), by = "age_chr") %>%
-    inner_join(select(gender_key, gender_chr, gender), by = "gender_chr") %>%
-    select(-education, -age_chr, -gender_chr) %>%
-    select(year:variable, gender, age, educ, everything())
+    mutate(frac_geo = count / count_geo) %>%
+    mutate(geo = geo) %>%
+    select(year, geo, matches("(st|cd)"), gender, age, educ, everything())
 }
+
 
 all_frac <- group_by(pop_st, year, variable) %>%
   summarize(count = sum(count)) %>%
-  clean_strat()
+  clean_strat("nat")
 
-st_frac <- clean_strat(group_by(pop_st, year, stid, state))
-cd_frac <- clean_strat(group_by(pop_cd, year, cdid, cd))
+st_frac <- clean_strat(group_by(pop_st, year, stid, state), "st")
+cd_frac <- clean_strat(group_by(pop_cd, year, cdid, cd), "cd")
 
+
+# Save --
 write_rds(all_frac, "data/output/by-national_ACS_gender-age-education.Rds")
 write_rds(cd_frac, "data/output/by-CD_ACS_gender-age-education.Rds")
 write_rds(st_frac, "data/output/by-ST_ACS_gender-age-education.Rds")
