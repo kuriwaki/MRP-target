09cae598d0e68f74d468916557e17bac996a5dc6
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-02 17:50:05 -0400

    refactor, allow for educ and race tables. Do some consolidation of categories, eg on age in race case

--- 02_format_ACS.R
+++ 02_format_ACS.R
@@ -7,21 +7,39 @@ pop_all <- read_rds("data/input/acs/by-all_acs_counts.Rds")
 pop_st <- read_rds("data/input/acs/by-st_acs_counts.Rds")
 pop_cd <- read_rds("data/input/acs/by-cd_acs_counts.Rds")
 
-# each user needs to get a Census key. See ??census_api_key
-# census_api_key("", install = TRUE)
-
-
+cc18 <- read_rds("data/input/by-person_cces-2018.Rds")
 
 # Recode variable df ------
-ages  <- c("18 to 24 years",
+ages5  <- c("18 to 24 years",
            "25 to 34 years",
            "35 to 44 years",
            "45 to 64 years",
            "65 years and over")
-age_lbl <- setNames(1:5L, ages)
-age_key <- tibble(age_chr = ages,
-                  age = labelled(1:5L, age_lbl))
+age5_lbl <- setNames(1:5L, ages5)
+age5_key <- tibble(age_chr = ages5,
+                  age = labelled(1:5L, age5_lbl))
 
+ages10 <- c("18 and 19 years",
+           "20 to 24 years",
+           "25 to 29 years",
+           "30 to 34 years",
+           "35 to 44 years",
+           "45 to 54 years",
+           "55 to 64 years",
+           "65 to 74 years",
+           "75 to 84 years",
+           "85 years and over")
+age10_key <- tibble(age_chr = ages10) %>%
+  mutate(age_num = case_when(
+    age_chr %in% ages10[1:2] ~ 1L,
+    age_chr %in% ages10[3:4] ~ 2L,
+    age_chr %in% ages10[5] ~ 3L,
+    age_chr %in% ages10[6:7] ~ 4L,
+    age_chr %in% ages10[8:10] ~ 5L
+  )) %>%
+  mutate(age = labelled(age_num, age5_lbl))
+
+# Education ----
 education <- c("Less than 9th grade",
                "9th to 12th grade, no diploma",
                "High school graduate \\(includes equivalency\\)",
@@ -31,12 +49,17 @@ education <- c("Less than 9th grade",
                "Graduate or professional degree")
 
 educ_lbl <- setNames(1L:7L,
-              c("Less than 9", "No HS", "High School Graduate", "Some College",
-              "2-Year", "4-Year", "Post-Grad"))
+                     c("Less than 9",
+                       "No HS",
+                       "High School Graduate",
+                       "Some College",
+                       "2-Year",
+                       "4-Year",
+                       "Post-Grad"))
 educ_lbl_cces <- setNames(1L:6L, names(educ_lbl)[2:7])
 
 
-# CCES lumps the first two
+## CCES lumps the first two
 cces_edlbl <- tibble(cces_label = names(educ_lbl)[2:7],
                    educ = labelled(1:6L, educ_lbl_cces))
 
@@ -47,62 +70,105 @@ educ_key  <- tibble(num = 1:7L,
   left_join(cces_edlbl, by = "cces_label") %>%
   select(-num)
 
+# Gender ----
 gender_key <- tibble(gender_chr = c("Male", "Female"),
                      gender = labelled(1:2L, c(Male = 1, Female = 2)))
 
-races <- c("White",
-           "Black",
-           "Hispanic",
-           "Asian",
-           "American Indian",
-           "Native Hawaiian",
-           "Other")
-race_key <- distinct(cc18, race) %>%
-  arrange(race) %>%
-  mutate(race_lbl = as.character(as_factor(race)))
+# Race -----
+my_racelbl <- setNames(1L:5L, c("White", "Black", "Hispanic", "Asian", "All Other"))
+
+race_cces_key <- distinct(cc18, race) %>%
+  mutate(race_cces_chr = as.character(as_factor(race))) %>%
+  rename(race_cces = race)
+
+race_key <- tribble(
+  ~race_5, ~race_cces_chr, ~race_acs,
+  1L, "White", "WHITE ALONE, NOT HISPANIC OR LATINO",
+  2L, "Black", "BLACK OR AFRICAN AMERICAN ALONE",
+  3L, "Hispanic", "HISPANIC OR LATINO",
+  4L, "Asian", "ASIAN ALONE",
+  5L, "Native American", "AMERICAN INDIAN AND ALASKA NATIVE ALONE",
+  5L, "Asian", "NATIVE HAWAIIAN AND OTHER PACIFIC ISLANDER ALONE",
+  5L, "Mixed", "TWO OR MORE RACES",
+  5L, "Other", "SOME OTHER RACE ALONE",
+  5L, "Middle Eastern", NA,
+) %>%
+  left_join(race_cces_key, by = "race_cces_chr") %>%
+  mutate(race = labelled(race_5, my_racelbl))
+race_key
 
 
 # Clean dataset as the combination of the two ----
-cell_vars <- vars %>%
+educ_cells <- vars %>%
   filter(!is.na(gender), !is.na(age), !is.na(educ)) %>%
-  rename(variable = name, gender_chr = gender, age_chr = age, educ_chr = educ) %>%
+  rename(gender_chr = gender, age_chr = age, educ_chr = educ) %>%
   left_join(gender_key, by = "gender_chr") %>%
-  left_join(age_key, by = "age_chr") %>%
+  left_join(age5_key, by = "age_chr") %>%
   left_join(educ_key, by = "educ_chr") %>%
   select(variable, gender, age, educ)
+
+race_cells <- vars %>%
+  filter(!is.na(gender), !is.na(age), !is.na(race)) %>%
+  rename(gender_chr = gender, age_chr = age, race_acs = race) %>%
+  left_join(gender_key, by = "gender_chr") %>%
+  left_join(age10_key, by = "age_chr") %>%
+  left_join(select(race_key, race_acs, race), by = "race_acs") %>%
+  select(variable, gender, age, race, race_acs)
 
 clean_strat <- function(grp_tab, geo, codes = cell_vars) {
   stopifnot(is.grouped_df(grp_tab)) # grouped by vars for geo
 
-  grp_tab %>%
-    mutate(count_geo = sum(count*(variable == "B15001_001"))) %>% # sum geo
-    inner_join(codes, by = "variable") %>% # drop marginal cells, so cells form partition
-    group_by(year, gender, age, educ, count_geo, add = TRUE) %>%  # drop variable code (let our labels preceed)
-    summarize(count = sum(count)) %>%
+  # grouping vars
+  if ("educ" %in% colnames(codes))
+    groups <- c("gender", "age", "educ")
+  if ("race" %in% colnames(codes))
+    groups <- c("gender", "age", "race")
+
+  groups <- syms(groups)
+
+  # add cces labels
+  keyd_tab <- grp_tab %>%
+    select(variable, year, count) %>%
+    inner_join(codes)
+
+  sum_geo <- keyd_tab %>%
+    summarize(count_geo = sum(count))
+
+  keyd_tab %>%
+    group_by(!!!groups, add = TRUE) %>%  # drop variable code (let our labels preceed)
+    summarize(count = sum(count)) %>% # count by somewhat coarser category
     ungroup() %>%
+    left_join(sum_geo) %>%  # add totals
     mutate(frac_geo = count / count_geo) %>%
+    filter(!is.na(frac_geo)) %>%
     mutate(geo = geo) %>%
-    select(year, geo, matches("(st|cd)"), gender, age, educ, everything())
+    select(year, geo, matches("(st|cd)"), gender, age, matches("race|educ"), everything())
 }
 
-all_frac <- group_by(pop_st, year, variable) %>%
+
+us_educ_frac <- group_by(pop_st, year, variable) %>%
   summarize(count = sum(count)) %>%
-  clean_strat("nat")
-us_frac <- pop_all %>%
-  group_by(year) %>%
-  clean_strat("nat")
-st_frac <- clean_strat(group_by(pop_st, year, stid, state), "st")
-cd_frac <- clean_strat(group_by(pop_cd, year, cdid, cd), "cd")
+  clean_strat("us", educ_cells)
 
-# Race marginals ---
-cd_frac
+us_race_frac <- group_by(pop_st, year, variable) %>%
+  summarize(count = sum(count, na.rm = TRUE)) %>%
+  clean_strat("us", race_cells)
 
+st_race_frac <- clean_strat(group_by(pop_st, year, stid, state), "st", race_cells)
+st_educ_frac <- clean_strat(group_by(pop_st, year, stid, state), "st", educ_cells)
+
+cd_race_frac <- clean_strat(group_by(pop_cd, year, cdid, cd), "cd", race_cells)
+cd_educ_frac <- clean_strat(group_by(pop_cd, year, cdid, cd), "cd", educ_cells)
 
 
 
 # Save --
-write_rds(all_frac, "data/output/by-national_ACS_gender-age-education.Rds")
-write_rds(cd_frac, "data/output/by-CD_ACS_gender-age-education.Rds")
-write_rds(st_frac, "data/output/by-ST_ACS_gender-age-education.Rds")
-save(age_key, gender_key, educ_key, educ_lbl, age_lbl,
+write_rds(us_educ_frac, "data/output/by-us_ACS_gender-age-education.Rds")
+write_rds(cd_educ_frac, "data/output/by-cd_ACS_gender-age-education.Rds")
+write_rds(st_educ_frac, "data/output/by-st_ACS_gender-age-education.Rds")
+write_rds(us_race_frac, "data/output/by-us_ACS_gender-age-race.Rds")
+write_rds(cd_race_frac, "data/output/by-cd_ACS_gender-age-race.Rds")
+write_rds(st_race_frac, "data/output/by-st_ACS_gender-age-race.Rds")
+
+save(age5_key, age10_key, gender_key, educ_key, race_key,
      file = "data/output/variable-labels.Rdata")
