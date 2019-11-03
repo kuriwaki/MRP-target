7a21fb923a10aa4be79a966056f3db2514f2c0a5
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-03 18:25:56 -0500

    shorten a bit, add race

--- 03_compare-cces-sample.R
+++ 03_compare-cces-sample.R
@@ -23,7 +23,37 @@ compute_fracs <- function(grptbl) {
            cces_sfrac = cces_sn / cces_sn_geo)
 }
 
+fracs_longer <- function(tbl) {
+  tbl %>%
+    pivot_longer(cols = -c(geo:acs_frac),
+                 values_to = "cces_frac",
+                 names_to  = "weight_type",
+                 names_pattern = "cces_(ufrac|wfrac|sfrac)") %>%
+    ungroup() %>%
+    mutate(geo_fct = recode_factor(as.character(geo),
+                                   us = "National",
+                                   st = "State-by-State",
+                                   cd = "CD-by-CD"),  # %>%
+           wgt_fct = recode_factor(as.character(weight_type),
+                                   `ufrac` = "Unweighted",
+                                   `wfrac` = "YouGov weights",
+                                   `sfrac` = "State-by-state rim weights"))
+}
 
+
+calc_errors <- function(tbl, accr = 0.1) {
+  pp <- unit_format(accuracy = accr, scale = 1e2, unit = "pp")
+  tbl %>%
+  group_by(geo, geo_fct, weight_type, wgt_fct) %>%
+    summarize(RMSE = sqrt(mean((acs_frac - cces_frac)^2)),
+              bias = mean(abs(acs_frac - cces_frac)),
+              n = n()) %>%
+    mutate(txt = str_c("RMSE: ", pp(RMSE), "\nBias: ", pp(bias))) %>%
+    filter(!is.na(txt)) %>%
+    ungroup()
+}
+
+# Data -----
 cc18_raw <- read_rds("data/input/by-person_cces-2018.Rds")
 weights  <- read_rds("data/output/weights-state.Rds")
 
@@ -111,43 +141,71 @@ comp_cd_race <- cd_race_frac %>%
 
 
 # stats on RMSE ---
-fracs_wide <- bind_rows(zap_labels(comp_us_educ),
-                        zap_labels(comp_st_educ),
-                        zap_labels(comp_cd_educ))
+fracs_wide_educ <- bind_rows(zap_labels(comp_us_educ),
+                             zap_labels(comp_st_educ),
+                             zap_labels(comp_cd_educ))
 
-fracs_long <- fracs_wide %>%
+fracs_long_edcell <- fracs_wide_educ %>%
   select(geo, cdid:cd, stid:state, gender, age, educ, matches("frac")) %>%
-  pivot_longer(cols = -c(geo:frac_geo),
-               values_to = "cces_frac",
-               names_to  = "weight_type",
-               names_pattern = "cces_(ufrac|wfrac|sfrac)") %>%
-  rename(frac_acs = frac_geo) %>%
-  ungroup() %>%
-  mutate(geo_fct = recode_factor(as.character(geo),
-                                 us = "National",
-                                 st = "State-by-State",
-                                 cd = "CD-by-CD"),  # %>%
-         wgt_fct = recode_factor(as.character(weight_type),
-                                 `ufrac` = "Unweighted",
-                                 `wfrac` = "YouGov weights",
-                                 `sfrac` = "State-by-state rim weights"))
+  fracs_longer()
 
-pp <- unit_format(accuracy = 0.01, scale = 1e2, unit = "pp")
-errs <- fracs_long %>%
-  group_by(geo, geo_fct, weight_type, wgt_fct) %>%
-  summarize(RMSE = sqrt(mean((frac_acs - cces_frac)^2)),
-            bias = mean(abs(frac_acs - cces_frac)),
-            n = n()) %>%
-  mutate(txt = str_c("RMSE: ", pp(RMSE), "\nBias: ", pp(bias))) %>%
-  filter(!is.na(txt)) %>%
-  ungroup()
-errs
+errs_edcell <- calc_errors(fracs_long_edcell, accr = 0.01)
+
+# marginalize over ed cells
+fracs_wide_educ_ed <-  fracs_wide_educ %>%
+  group_by(geo, state, cd, educ) %>%
+  summarize(acs_frac = sum(count) / unique(count_geo),
+            cces_ufrac = sum(cces_n) / unique(cces_n_geo),
+            cces_wfrac = sum(cces_wn) / unique(cces_wn_geo),
+            cces_sfrac = sum(cces_sn) / unique(cces_sn_geo))
+
+ed_long <- fracs_wide_educ_ed %>%
+  fracs_longer() %>%
+  left_join(distinct(transmute(educ_key,
+                               educ_fct = as_factor(educ),
+                               educ = as.integer(educ))))
+
+errs_ed <- calc_errors(ed_long)
+
+# what about margins over race?
+fracs_wide_race <- bind_rows(zap_labels(comp_us_race),
+                             zap_labels(comp_st_race),
+                             zap_labels(comp_cd_race))
+
+fracs_wide_race_re <-  fracs_wide_race %>%
+  group_by(geo, state, cd, race) %>%
+  summarize(acs_frac = sum(count) / unique(count_geo),
+            cces_ufrac = sum(cces_n) / unique(cces_n_geo),
+            cces_wfrac = sum(cces_wn) / unique(cces_wn_geo),
+            cces_sfrac = sum(cces_sn) / unique(cces_sn_geo)) %>%
+  arrange(geo)
 
 
-# get a sense of relationship ----
-gg_cell_temp <- fracs_long  %>%
+re_long <- fracs_wide_race_re %>%
+  fracs_longer() %>%
+  left_join(distinct(transmute(race_key,
+                               race_fct = as_factor(race),
+                               race = as.integer(race))))
+
+errs_re <-  calc_errors(re_long, 0.1)
+
+gg_cell_temp %+% re_long +
+  scale_x_continuous(limits = c(0,1), breaks = seq(0, 1, 0.25), labels = percent_format(accuracy = 1)) +
+  scale_y_continuous(limits = c(0,1), breaks = seq(0, 1, 0.25), labels = percent_format(accuracy = 1)) +
+  facet_rep_grid(wgt_fct ~ geo_fct, repeat.tick.labels = TRUE) +
+  geom_text(data = mutate_if(errs_re, is.labelled, as_factor),
+            aes(x = 0.25, y = 0.90, label = txt), size = 3)
+
+# explore race
+# fracs_wide_race_re %>%
+#   filter(race == 1) %>%
+#   arrange(desc(acs_frac))
+
+
+# plot ----
+gg_cell_temp <- fracs_long_edcell  %>%
   filter(weight_type %in% c("ufrac", "wfrac")) %>%
-  ggplot(aes(frac_acs, cces_frac)) +
+  ggplot(aes(acs_frac, cces_frac)) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed", alpha = 0.5) +
   coord_equal() +
   geom_point(aes(size = geo_fct, alpha = geo_fct)) +
@@ -170,65 +228,28 @@ gg_cell_temp <- fracs_long  %>%
 
 gg_cell_educ <- gg_cell_temp +
   facet_rep_grid(wgt_fct ~ geo_fct, repeat.tick.labels = TRUE) +
-  geom_text(data = mutate_if(filter(errs, wgt_fct %in% c("Unweighted", "YouGov weights")), is.labelled, as_factor),
+  geom_text(data = mutate_if(filter(errs_edcell, wgt_fct %in% c("Unweighted", "YouGov weights")), is.labelled, as_factor),
             aes(x = 0.08, y = 0.02, label = txt), size = 3)
 
 ggsave("figures/cellfrac-comparisons.pdf", gg_cell_educ, h = 5 + 1.2, w = 5*1.5 + 0.8)
 ggsave("figures/cellfrac-comparisons.png", gg_cell_educ, h = 5 + 1.2, w = 5*1.5 + 0.8)
 
-gg_cell_rim <- gg_cell_temp %+% as_factor(filter(fracs_long, wgt_fct == "State-by-state rim weights"))  +
+gg_cell_rim <- gg_cell_temp %+%
+  as_factor(filter(fracs_long_edcell, wgt_fct == "State-by-state rim weights"))  +
   facet_rep_grid( ~ geo_fct, repeat.tick.labels = TRUE) +
-  geom_text(data = as_factor(filter(errs, weight_type %in% "sfrac")),
+  geom_text(data = as_factor(filter(errs_edcell, weight_type %in% "sfrac")),
             aes(x = 0.08, y = 0.02, label = txt), size = 3) +
   labs(caption = "CCES uses custom state-by-state rim weights.") +
   guides(color = FALSE)
 
-ggsave("figures/cellfrac-rim-comparisons.png", h = 3, w = 2.5*3)
-ggsave("figures/cellfrac-rim-comparisons.pdf", h = 3, w = 2.5*3)
+ggsave("figures/cellfrac-rim-comparisons.png", gg_cell_rim, h = 3 + 0.2, w = 2.5*3)
 
 
-# education only ----
-# paste earlier, but without age and education
-# unweighted raw counts
-
-cces_count_ed <-  fracs_wide %>%
-  group_by(educ, geo, state, cd) %>%
-  summarize(acs_frac = sum(count) / unique(count_geo),
-            cces_frac = sum(cces_n) / unique(cces_n_geo),
-            cces_wfrac = sum(cces_wn) / unique(cces_wn_geo),
-            cces_sfrac = sum(cces_sn) / unique(cces_sn_geo))
-
-ed_long <- cces_count_ed %>%
-  pivot_longer(cols = -c(educ, geo:acs_frac),
-               values_to = "cces_frac",
-               names_to  = "weight_type",
-               names_pattern = "cces_(frac|wfrac|sfrac)") %>%
-  ungroup() %>%
-  mutate(geo_fct = recode_factor(geo,
-                                 us = "National",
-                                 st = "State-by-State",
-                                 cd = "CD-by-CD"),  # %>%
-         wgt_fct = to_labelled(recode_factor(as.character(weight_type),
-                                 `frac` = "Unweighted",
-                                 `wfrac` = "YouGov weights",
-                                 `sfrac` = "State-by-state rim weights"))) %>%
-  left_join(distinct(transmute(educ_key,
-                               educ_fct = as_factor(educ),
-                               educ = as.integer(educ))))
-
-
-pp <- unit_format(accuracy = 0.1, scale = 1e2, unit = "pp")
-errs_ed <- ed_long %>%
-  group_by(geo_fct, wgt_fct) %>%
-  summarize(RMSE = sqrt(mean((acs_frac - cces_frac)^2)),
-            bias = mean(abs(acs_frac - cces_frac)),
-            n = n()) %>%
-  mutate(txt = str_c("RMSE: ", pp(RMSE), "\nBias: ", pp(bias))) %>%
-  ungroup()
+# plot ----
 
 
 gg_ed_temp <- ed_long  %>%
-  filter(wgt_fct %in% 1:2) %>%
+  filter(weight_type %in% c("ufrac", "wfrac")) %>%
   mutate(wgt_fct = as_factor(wgt_fct)) %>%
   ggplot(aes(acs_frac, cces_frac)) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed", alpha = 0.5) +
@@ -256,14 +277,15 @@ gg_ed_temp <- ed_long  %>%
 
 gg_ed_uw <-  gg_ed_temp +
   facet_rep_grid(wgt_fct ~ geo_fct, repeat.tick.labels = TRUE) +
-  geom_text(data = mutate_if(filter(errs_ed, wgt_fct %in% 1:2), is.labelled, as_factor),
+  geom_text(data = mutate_if(filter(errs_ed, weight_type %in% c("ufrac", "wfrac")),
+                             is.labelled, as_factor),
             aes(x = 0.1, y = 0.4, label = txt), size = 3)
 
 ggsave("figures/educfrac-comparisons.pdf", gg_ed_uw, h = 5 + 1.2, w = 5*1.5)
 
-gg_ed_rim <- gg_ed_temp %+% as_factor(filter(ed_long, wgt_fct == 3))  +
+gg_ed_rim <- gg_ed_temp %+% as_factor(filter(ed_long, weight_type %in% "sfrac"))  +
   facet_rep_grid( ~ geo_fct, repeat.tick.labels = TRUE) +
-  geom_text(data = as_factor(filter(errs_ed, wgt_fct %in% 3)),
+  geom_text(data = as_factor(filter(errs_ed, weight_type %in% "sfrac")),
             aes(x = 0.1, y = 0.4, label = txt), size = 3) +
   labs(caption = "CCES uses custom state-by-state rim weights.") +
   guides(color = FALSE)
