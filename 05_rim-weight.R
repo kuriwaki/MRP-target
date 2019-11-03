313564bd5dce3c4b71bf32e3523b5a05cc230f6f
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-02 23:32:47 -0400

    only look at state by state weights

--- 05_rim-weight.R
+++ 05_rim-weight.R
@@ -10,12 +10,12 @@ library(ddi)
 cc18 <- read_rds("data/input/by-person_cces-2018.Rds") %>%
   mutate(g_a_e = str_c(gender, age, educ))
 
-cd_frac <- read_rds("data/output/by-CD_ACS_gender-age-education.Rds")
-st_frac <- read_rds("data/output/by-ST_ACS_gender-age-education.Rds")
+cd_frac <- read_rds("data/output/by-cd_ACS_gender-age-education.Rds")
+st_frac <- read_rds("data/output/by-st_ACS_gender-age-education.Rds")
 
 
-cd_clinton <- gs_read(gs_title("wasserman_2018-2016_CD")) %>%
-  mutate(clinton_2pty = (margin_clinton + 1)/2)
+# cd_clinton <- gs_read(gs_title("wasserman_2018-2016_CD")) %>%
+  # mutate(clinton_2pty = (margin_clinton + 1)/2)
 
 data(g2016)
 
@@ -26,6 +26,8 @@ targets_st <- st_frac %>%
 u_states <- unique(targets_st$state)
 u_stids  <- unique(targets_st$stid)
 
+
+if (FALSE) {
 w_df_st <- matrix(NA,
                nrow = nrow(cc18),
                ncol = length(u_stids),
@@ -96,6 +98,7 @@ for (dist_i in u_cdids) {
   w_df_cd[, dist_i] <- df_wgt$weight_cd
 }
 
+} # end multicolumn weighting
 
 # state partitioned weights -----
 targets_st <- st_frac %>%
@@ -126,21 +129,22 @@ st_par_w <- foreach(st_i = u_states, .combine = "bind_rows") %do% {
     data = filter(cc18, state == st_i) %>% mutate_if(is.labelled, zap_labels),
     category(name = "gender", prop_i_gender$gender, targets = prop_i_gender$count, sum.1 = TRUE),
     category(name = "age", prop_i_age$age, targets = prop_i_age$count, sum.1 = TRUE),
-    category(name = "educ", prop_i_educ$educ, targets = prop_i_educ$count, sum.1 = TRUE),
-    category(name = "trump", prop_i_educ$educ, targets = prop_i_educ$count, sum.1 = TRUE)
+    category(name = "educ", prop_i_educ$educ, targets = prop_i_educ$count, sum.1 = TRUE)
     )
 
   df_wgt <- iterake(universe = uni,
                     max.iter = 100,
                     max.wgt = 15,
-                    wgt.name = "weight_st")
+                    wgt.name = "weight_st",
+                    summary = FALSE)
 
   select(df_wgt, case_id, weight, weight_st)
 }
 
 st_par_w %>%
   ggplot(aes(weight, weight_st)) +
-  geom_point(size = 0.1)
+  geom_point(size = 0.1) +
+  coord_equal()
 
 write_rds(st_par_w, "data/output/weights-state.Rds")
 
