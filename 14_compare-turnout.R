f41c31b934d5e669d53af548d89c06b7c7eaeaa8
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-13 10:54:41 -0500

    compute coverage and SEs to mrp

--- 14_compare-turnout.R
+++ 14_compare-turnout.R
@@ -18,39 +18,39 @@ name_models <- function(string) {
 }
 
 # Data ----
-cces_cd  <- read_rds("data/output/by-cd-issue_all-estimates_sd-01.Rds")
+cces_cd  <- read_rds("data/output/by-cd-issue_all-estimates_sd-02.Rds")
 cd_votes <- read_rds("data/output/by-cd_CVAP-turnout.Rds")
 
 # Melted versions
 rel_all <- pivot_longer(cces_cd,
-                        -c(q_label, cd),
-                        names_pattern = "(n|p|se)_(raw|yg|st|mrp)",
+                        -c(q_label, cd, matches("p_mrp_[0-9+]")),
+                        names_pattern = "(n|p|se)_(raw|yg|st|mrp_est)",
                         names_to = c(".value", "model")) %>%
+  mutate(model = recode(model, mrp_est = "mrp")) %>%
   reord_m()
 
 
 # turnout ---
-rel_all %>%
-  group_by(model) %>%
-  summarize(var = var(p))
 
 cd_df <- left_join(rel_all, cd_votes, by = "cd") %>%
   filter(q_label == "Turnout") %>%
   filter(turnout_cvap > 0.01) %>%
   mutate(p_lb = p - qnorm(0.975)*se,
-         p_ub = p + qnorm(0.975)*se)
+         p_ub = p + qnorm(0.975)*se) %>%
+  mutate(p_lb = coalesce(p_lb, p_mrp_025),
+         p_ub = coalesce(p_ub, p_mrp_975))
 
 
 # diagnostics
 rmse_cd <- cd_df %>%
   group_by(q_label, model) %>%
   rmse(turnout_cvap, p) %>%
-  mutate(txt = str_c("RMSE: ", format(.estimate*100, digits = 1, nsmall = 1), "pp"))
+  mutate(rmse_txt = str_c("RMSE: ", format(.estimate*100, digits = 1, nsmall = 1), "pp"))
 
 bias_cd <- cd_df %>%
   group_by(q_label, model) %>%
   mae(turnout_cvap, p)  %>%
-  mutate(txt = str_c("MAD: ", format(.estimate*100, digits = 1, nsmall = 1), "pp"))
+  mutate(mad_txt = str_c("MAD: ", format(.estimate*100, digits = 1, nsmall = 1), "pp"))
 
 avgbias_cd <- cd_df %>%
   group_by(q_label, model) %>%
@@ -66,17 +66,17 @@ cvg_cd <- cd_df %>%
 cd_bind <- left_join(rmse_cd, bias_cd, by = c("q_label", "model")) %>%
   left_join(avgbias_cd) %>%
   left_join(cvg_cd) %>%
-  transmute(q_label, model, txt = str_c(txt.x, "\n", bias_txt, "\n", replace_na(cvg_txt, "")))
+  transmute(q_label, model, txt = str_c(rmse_txt, "\n", mad_txt, "\n", replace_na(cvg_txt, "")))
 
 
 ggplot(cd_df, aes(turnout_cvap, p)) +
   geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
   geom_point(size = 0.5, alpha = 0.25) +
-  geom_errorbar(aes(ymin = p_lb, ymax = p_ub), width = 0, alpha = 0.05) +
+  geom_errorbar(aes(ymin = p_lb, ymax = p_ub), width = 0, alpha = 0.10) +
   facet_wrap(~ model, labeller = labeller(model = name_models)) +
-  scale_x_continuous(labels = percent_format(accuracy = 1), limits = c(0.15, 0.85), minor_breaks = NULL) +
-  scale_y_continuous(labels = percent_format(accuracy = 1), limits = c(0.15, 0.85), minor_breaks = NULL) +
-  geom_text(data = cd_bind, aes(x = 0.35, y = 0.70, label = txt), size = 3)  +
+  scale_x_continuous(labels = percent_format(accuracy = 1), limits = c(0, 1),  minor_breaks = NULL) +
+  scale_y_continuous(labels = percent_format(accuracy = 1), limits = c(0, 1),  minor_breaks = NULL) +
+  geom_text(data = cd_bind, aes(x = 0.40, y = 0.80, label = txt), size = 3)  +
   coord_equal() +
   theme_classic() +
   labs(x = "CVAP Turnout (House Vote / 2017 ACS CVAP Count)",
