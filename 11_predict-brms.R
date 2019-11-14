9ef10636a42ac61063abf9e37b044237bffbdc27
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-13 19:23:50 -0500

    use linpred

--- 11_predict-brms.R
+++ 11_predict-brms.R
@@ -33,14 +33,14 @@ cd_strat_raw <- read_rds("data/output/by-cd_ACS_gender-age-education.Rds") %>%
 outcomes <- c("ahca", "budg", "immr", "visa", "tcja", "sanc", "turn")
 
 
-for (sd in c("01", "02", "05")[1]) {
+for (sd in c("default", "01", "02", "05")[1]) {
   cellfiles <- dir_ls(glue("data/output/stan_glmer/sd-{sd}"), recurse = TRUE)
   outcomes_s <- unique(str_extract(cellfiles, str_c("(", str_c(outcomes, collapse = "|"), ")")))
 
-  for (y in outcomes_s[1]) { #c(outcomes_s)) {
+  for (y in "turn") { #c(outcomes_s)) {
     var_name <- glue("n_{y}")
 
-    fit <- read_rds(glue("data/output/stan_glmer/sd-{sd}/by-cd_{y}_g-a-e_brm.Rds"))
+    fit <- read_rds(glue("data/output/stan_glmer/sd-{sd}/by-cd_{y}_g-a-e_glmer.Rds"))
 
     # prediced ----
     all_strat <- cd_strat_raw %>%
@@ -53,8 +53,9 @@ for (sd in c("01", "02", "05")[1]) {
       cd_strat <- filter(all_strat, cd == cd_i)
 
       # no longer works with stan_glmer
-      p_draws <- posterior_predict(fit,
+      p_draws <- posterior_linpred(fit,
                         newdata = cd_strat,
+                        transform = TRUE,
                         allow_new_levels = TRUE,
                         summary = FALSE) %>%
         t() %>%
@@ -65,7 +66,7 @@ for (sd in c("01", "02", "05")[1]) {
         mutate(iter = parse_number(iter))
 
       cd_est <- group_by(p_draws, cd, iter) %>%
-        summarize(p_mrp = sum(value) / sum(.data[[var_name]]))
+        summarize(p_mrp = sum(value*.data[[var_name]]) / sum(.data[[var_name]]))
 
       write_rds(cd_est,
                 glue("data/output/cd-estimates/stan_glmer/sd-{sd}/{y}/{cd_i}_gae_brm-preds.Rds"))
