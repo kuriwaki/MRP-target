e791c920bca0ec483ded9d5932bf6458959b76d7
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-11-15 09:22:35 -0500

    have a default prior option too

--- 10_fit-mrp.R
+++ 10_fit-mrp.R
@@ -4,17 +4,14 @@ library(rstanarm)
 library(fs)
 library(glue)
 
+source("00_functions.R")
+
 resp_18 <- read_rds("data/input/by-question_cces-2018.Rds")
 cd_frac_educ <- read_rds("data/output/by-cd_ACS_gender-age-education.Rds")
 
-transform_vars <- function(tbl) {
-  tbl %>%
-  mutate(male = -0.5 + 1 * (as_factor(gender) == "Male"),
-         educ = as_factor(educ),
-         age = as_factor(age))
-}
 
 wide_18 <- resp_18 %>%
+  filter(citizen == 1) %>%
   select(case_id, weight, cd, gender, age:marstat, vv_turnout_gvm, q_label, response) %>%
   pivot_wider(id_cols = case_id:vv_turnout_gvm,
               names_from = q_label,
@@ -64,7 +61,7 @@ ff_base <- "cbind(ahca, n_ahca - ahca) ~ male +
 outcomes <- c("turn", "ahca", "sanc", "immr", "tcja", "budg", "visa")
 
 
-fit_outcome <- function(outcome, data = cd_binomial, base_formula = ff_base, sd = 1){
+fit_outcome <- function(outcome, data = cd_binomial, base_formula = ff_base, sd = "default"){
   var <- enquo(outcome)
   var_name <- quo_name(var)
   nvar_name <- str_c("n_", var_name)
@@ -74,29 +71,34 @@ fit_outcome <- function(outcome, data = cd_binomial, base_formula = ff_base, sd 
   data_nzero <- data %>%
     filter(!!sym(nvar_name) != 0)
 
-  fit <- stan_glmer(ff_outcome,
-                    data = data_nzero,
-                    family = binomial,
-                    chains = 4,
-                    cores = 4,
-                    prior_PD = TRUE,
-                    prior = normal(location = 0, scale = sd),
-                    prior_intercept = normal(location = 0, scale = sd),
-                    prior_aux = normal(location = 0, scale = sd),
-                    seed = 02138)
+  if (sd != "default") {
+    fit <- stan_glmer(ff_outcome,
+                      data = data_nzero,
+                      family = binomial,
+                      chains = 4,
+                      cores = 4,
+                      prior_PD = TRUE,
+                      prior = normal(location = 0, scale = sd),
+                      prior_intercept = normal(location = 0, scale = sd),
+                      prior_aux = normal(location = 0, scale = sd),
+                      seed = 02138)
+  }
+  if (sd == "default") {
+    fit <- stan_glmer(ff_outcome,
+                      data = data_nzero,
+                      family = binomial,
+                      chains = 4,
+                      cores = 4,
+                      prior_PD = TRUE,
+                      seed = 02138)
+  }
 
-  write_rds(fit, path("data/output/stan_glmer", glue("sd-{str_pad(sd, 2, pad = '0')}/by-cd_{outcome}_g-a-e_brm.Rds")))
+  write_rds(fit, path("data/output/stan_glmer", glue("sd-{str_pad(sd, 2, pad = '0')}/by-cd_{outcome}_g-a-e_glmer.Rds")))
   write_rds(data_nzero, path("data/output/stan_glmer", glue("sd-{str_pad(sd, 2, pad = '0')}/by-cd_{outcome}_g-a-e_df.Rds")))
 }
 
+fit_outcome("turn", sd = "default")
 fit_outcome("turn", sd = 1)
 fit_outcome("turn", sd = 2)
 fit_outcome("turn", sd = 5)
-# fit_outcome("sanc")
-# fit_outcome("ahca")
-# fit_outcome("budg")
-# fit_outcome("visa")
-# fit_outcome("immr")
-# fit_outcome("tcja")
-# fit_outcome("turn", sd = 10)
 
