ea937730890928f6eea3dbc2ba98c1cc8ded0b80
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2019-12-08 19:03:15 -0500

    more dor

--- 00_create-folders.R
+++ 00_create-folders.R
@@ -14,7 +14,7 @@ dir_create("data/output/reg")
 for (output in c("CDs", "reg")) {
   for (model in c("stan_glmer", "brms")) {
     for (sd in c("01", "02", "05", "10", "default", "hanretty")) {
-      for (outcome in c("turn", "ahca", "sanc"))
+      for (outcome in c("turn", "turn-vshare", "ahca", "sanc"))
         dir_create(path("data", "output",  output, model, str_c("sd-", sd), outcome))
     }
   }
