a9cce4dc4df021b1b3cefaba08c05520cc1f104a
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2020-04-02 14:52:04 -0400

    more up to date data

--- prepare/cces_slim.R
+++ prepare/cces_slim.R
@@ -9,20 +9,19 @@ dir <- "~/Dropbox/CCES_representation/"
 response <- readRDS(path(dir, "data/output/intermediate/by-response_response.Rds"))
 person   <- readRDS(path(dir, "data/output/intermediate/by-person_covariates.Rds")) %>%
   filter(year == 2018)
-cc18_all <- read_dta("~/Dropbox/cces_cumulative/data/source/cces/2018_cc.dta")
 
-citizen <- cc18_all %>%
-  transmute(case_id = as.integer(caseid), citizen = cit1)
+ccc_dir <- "~/Dropbox/cces_cumulative/data/release"
+ccc <- read_rds(path(ccc_dir, "cumulative_2006_2019_addon.Rds"))
 
 # joined
-cc18_fmt <- person %>%
-  left_join(citizen) %>%
+cc18_fmt <- ccc %>%
+  filter(year == 2018) %>%
   ccc_std_demographics()
-
-# merge back on to response data
-resp_18 <- inner_join(response, cc18_fmt)
 
 
 # Save ----
 write_rds(cc18_fmt, "data/input/by-person_cces-2018.Rds")
+
+# merge back on to response data
+resp_18 <- inner_join(response, cc18_fmt)
 write_rds(resp_18, "data/input/by-question_cces-2018.Rds")
